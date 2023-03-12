2973ed02cde8c594ba4793ce6c88c11408e8c0d8
chace <chacenb@gmail.com>
2023-03-12 06:27:05 +0100

    ...

--- serverManagement/src/main/java/com/chace/serverManagement/ServerManagementApplication.java
+++ serverManagement/src/main/java/com/chace/serverManagement/ServerManagementApplication.java
@@ -3,6 +3,7 @@ package com.chace.serverManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ServerManagementApplication {
 
