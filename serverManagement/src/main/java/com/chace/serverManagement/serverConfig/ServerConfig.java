2487bb62c805b6b1d0c4b4a43860676ebf2738e8
chace <chacenb@gmail.com>
2023-04-03 03:19:53 +0100

    application.yml changed from create-drop to update

--- serverManagement/src/main/java/com/chace/serverManagement/serverConfig/ServerConfig.java
+++ serverManagement/src/main/java/com/chace/serverManagement/serverConfig/ServerConfig.java
@@ -13,14 +13,14 @@ import static com.chace.serverManagement.enumeration.Status.SERVER_UP;
 
 @Configuration
 public class ServerConfig {
-    @Bean
-    CommandLineRunner startupConfig(ServerRepo serverRepo) {
-        return args -> {
-            Server serv1 = new Server(null, "192.168.1.150", "Ubuntu Server", "32 GB", "Personal PC", "http://localhost:8080/api/v2/server/image/serv0.png", SERVER_UP);
-            Server serv2 = new Server(null, "192.168.2.103", "Microsoft Server 2019", "64 GB", "CISCO", "http://localhost:8080/api/v2/server/image/serv1.png", SERVER_DOWN);
-            Server serv3 = new Server(null, "192.168.1.39", "Debian", "16 GB", "Remote", "http://localhost:8080/api/v2/server/image/serv2.png", SERVER_DOWN);
-            Server serv4 = new Server(null, "192.168.2.1", "Gateway", "64 GB", "Remote", "http://localhost:8080/api/v2/server/image/serv4.png", SERVER_DOWN);
-            serverRepo.saveAll(List.of(serv1, serv2, serv3, serv4));
-        };
-    }
+//    @Bean
+//    CommandLineRunner startupConfig(ServerRepo serverRepo) {
+//        return args -> {
+//            Server serv1 = new Server(null, "192.168.1.150", "Ubuntu Server", "32 GB", "Personal PC", "http://localhost:8080/api/v2/server/image/serv0.png", SERVER_UP);
+//            Server serv2 = new Server(null, "192.168.2.103", "Microsoft Server 2019", "64 GB", "CISCO", "http://localhost:8080/api/v2/server/image/serv1.png", SERVER_DOWN);
+//            Server serv3 = new Server(null, "192.168.1.39", "Debian", "16 GB", "Remote", "http://localhost:8080/api/v2/server/image/serv2.png", SERVER_DOWN);
+//            Server serv4 = new Server(null, "192.168.2.1", "Gateway", "64 GB", "Remote", "http://localhost:8080/api/v2/server/image/serv4.png", SERVER_DOWN);
+//            serverRepo.saveAll(List.of(serv1, serv2, serv3, serv4));
+//        };
+//    }
 }
