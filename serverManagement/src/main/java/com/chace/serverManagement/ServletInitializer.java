4f7631737396996e79a9b9e0ba4e54e7452f92db
chacenb <57180504+chacenb@users.noreply.github.com>
2024-06-02 08:50:18 +0100

    Update ServletInitializer.java
    Added comment on why we need the servlet initializer

--- serverManagement/src/main/java/com/chace/serverManagement/ServletInitializer.java
+++ serverManagement/src/main/java/com/chace/serverManagement/ServletInitializer.java
@@ -3,6 +3,13 @@ package com.chace.serverManagement;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/* WHY DO WE NEED THIS CLASS > https://howtodoinjava.com/spring-boot/spring-boot-servletinitializer/
+*****************************************************
+* Spring Boot provides SpringBootServletInitializer that runs a Spring Application "FROM A WAR DEPLOYMENT". 
+* It binds Servlet, Filter and ServletContextInitializer beans from the spring application context to the server.
+* Note that a WebApplicationInitializer is only needed IF WE BUILD A WAR FILE AND DEPLOY IT. 
+* We won’t need this if we run an embedded web server.
+**/
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
