5804713e58e29a7c496934363715368b5c83f95a
b.tchassem <b.tchassem@sahelys.com>
2024-06-21 16:43:36 +0100

    Fully implemented entity=Dto Mapping with modelMapper dependency

--- serverManagement/src/main/java/com/chace/serverManagement/Model/utils/ServerMapper.java
+++ serverManagement/src/main/java/com/chace/serverManagement/Model/utils/ServerMapper.java
@@ -15,25 +15,19 @@ public class ServerMapper {
   /* Standard guide to modelMapping using ModelMapper > https://www.baeldung.com/java-modelmapper */
   private final ModelMapper modelMapper = new ModelMapper();
 
-  // #1- When there is a difference between entity and DTO, setup fields mapping
-  TypeMap<Server, ServerDTO> serverDtoTypeMap; // = this.modelMapper.createTypeMap(Server.class, ServerDTO.class);
-  TypeMap<ServerDTO, Server> dtoServerTypeMap; // = this.modelMapper.createTypeMap(ServerDTO.class, Server.class);
+  // #1- When there is a difference between entity and DTO, configure the fields mapping
+  TypeMap<Server, ServerDTO> serverDtoTypeMap = this.modelMapper.createTypeMap(Server.class, ServerDTO.class);
+  TypeMap<ServerDTO, Server> dtoServerTypeMap = this.modelMapper.createTypeMap(ServerDTO.class, Server.class);
 
   @PostConstruct
   public void initializeTypeMaps() {
 
-    // #1- When there is a difference between entity and DTO, setup fields mapping and add the different mappings
-    serverDtoTypeMap = this.modelMapper.createTypeMap(Server.class, ServerDTO.class);
+    // #2 - And add the different mappings
     serverDtoTypeMap.addMapping(Server::getId, ServerDTO::set_id);
     serverDtoTypeMap.addMapping(Server::getIpAddress, ServerDTO::set_ipAddress);
-
-    dtoServerTypeMap = this.modelMapper.createTypeMap(ServerDTO.class, Server.class);
+    /*------------*/
     dtoServerTypeMap.addMapping(ServerDTO::get_id, Server::setId);
     dtoServerTypeMap.addMapping(ServerDTO::get_ipAddress, Server::setIpAddress);
-
-    log.info("---------- the modelMapper = {}", modelMapper.getConfiguration().toString());
-    log.info("---------- the serverDtoTypeMap = {}", serverDtoTypeMap.getMappings());
-    log.info("---------- the dtoServerTypeMap = {}", dtoServerTypeMap.getMappings());
   }
 
   public ServerDTO toDTO(Server server) {
@@ -45,20 +39,10 @@ public class ServerMapper {
   }
 
   public ServerDTO toDTO2(Server server) {
-//    //    When there is a difference between entity and DTO, setup fields mapping
-//    serverDtoTypeMap.addMapping(Server::getId, ServerDTO::set_id);
-//    serverDtoTypeMap.addMapping(Server::getIpAddress, ServerDTO::set_ipAddress);
-
     return this.modelMapper.map(server, ServerDTO.class);
   }
 
-  public Server toEntity2(ServerDTO serverDTO) {
-//    //    When there is a difference between entity and DTO, setup fields mapping
-//    dtoServerTypeMap.addMapping(ServerDTO::get_id, Server::setId);
-//    dtoServerTypeMap.addMapping(ServerDTO::get_ipAddress, Server::setIpAddress);
-
-    return this.modelMapper.map(serverDTO, Server.class);
-  }
+  public Server toEntity2(ServerDTO serverDTO) {return this.modelMapper.map(serverDTO, Server.class);}
 
 
 }
